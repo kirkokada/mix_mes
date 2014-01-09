0756d4b45afea3c9f5cf0e468bbfa7870557be26
Kirk Okada <kirkokada@gmail.com>
2014-01-09 19:11:42 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MixMes::Application.config.secret_key_base = 'dc25c4d778a1aee09fb4e1c8e46e59f3a54d6c68b47b1311124cb6c5b25277660a33b41ecd379a2dcde78ed207d686dd953cacab36f7808c46ff6fbc9d4cc0ab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MixMes::Application.config.session_store :cookie_store, key: '_mix_mes_session'
